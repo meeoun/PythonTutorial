b3bcbd5d23e47a1ff5946485c2763ad22977dec3
Kitt Parker <kparker@conciseit.net>
2021-01-17 20:55:16 -0500

    Section Two

---
+++ Section Two/exercises/Answers/Task_Four_Answer.py
@@ -0,0 +1,4 @@
+# This is impossible.  Integers are Immutable.
+# Changing a value will create a new data object.
+# If you missed this question, you should understand
+# mutable/immutable now =)
---
+++ Section Two/exercises/Answers/Task_Three_Answer.py
@@ -0,0 +1,6 @@
+# c variable points to the same data object as a
+# d variable points to the same data object as b
+a = 5
+b = 6
+c = a
+d = b
