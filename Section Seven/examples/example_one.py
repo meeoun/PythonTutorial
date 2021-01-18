786ad57b7ebeab30df301778993fc16ad6c2de39
Kitt Parker <kparker@conciseit.net>
2021-01-18 11:56:43 -0500

    Section Seven

---
+++ Section Seven/examples/example_one.py
@@ -0,0 +1,15 @@
+# Empty Tuple
+a = ()
+
+# Tuple with one item 
+a = ('one',)
+
+# Tuple with two items 
+b = ('one', 'two')
+
+# Combining Tuples
+a = a + b 
+
+print(a)
+
+# how many data objects where created?
