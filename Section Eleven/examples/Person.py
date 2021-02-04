fac16e981670058b71b73aa57a47f9253a9af709
Kitt Parker <kparker@conciseit.net>
2021-02-04 15:56:19 -0500

    Section Twelve

--- Section Eleven/examples/Person.py
+++
@@ -1,4 +0,0 @@
-class Person:
-    def __init__(self):
-        self.first_name = "John"
-        self.last_name = "Smith"
--- Section Eleven/examples/example/Animal.py
+++
@@ -1,4 +0,0 @@
-class Animal:
-    def __init__(self):
-        self.width = 10
-        self.hieght = 10
